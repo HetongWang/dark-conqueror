ae769fa9970437100e275c2c0e4cde86f6c4a23a
Tonghe Wang <hetong583@qq.com>
2015-10-15 00:45:56 -0700

    refactor code

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -4,10 +4,26 @@ using System.Collections;
 public class Character : MonoBehaviour {
     [HideInInspector]
     public bool facingRight = true;
+    public float moveSpeed = 4f;
 
-    public float maxSpeed = 4f;
+    public float jumpForce = 600;
+    protected Transform groundCheck;
+    protected bool grounded = false;
 
     public int hp = 10;
+    protected Rigidbody2D body;
+
+    public virtual void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("groundCheck");
+    }
+
+    public virtual void Update()
+    {
+        // Check if grounded
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
 
     protected void Flip()
     {
@@ -33,4 +49,37 @@ public class Character : MonoBehaviour {
             Destroy(gameObject);
     }
 
+    public void move(float horInput)
+    {
+        // Control horizontal speed
+        if (horInput != 0)
+            body.velocity = new Vector2(Mathf.Sign(horInput) * moveSpeed, body.velocity.y);
+        else
+        {
+            body.velocity = new Vector2(0, body.velocity.y);
+        }
+
+        // Change facing direct
+        if ((horInput > 0 && !facingRight) || (horInput < 0 && facingRight))
+        {
+            Flip();
+        }
+    }
+
+    public void move(float horInput, float verInput)
+    {
+        move(horInput);
+        if (verInput != 0)
+            body.velocity = new Vector2(body.velocity.x, verInput * moveSpeed);
+        else
+        {
+            body.velocity = new Vector2(body.velocity.x, 0);
+        }
+    }
+
+    public void jump()
+    {
+        if (grounded)
+            body.AddForce(new Vector2(0, jumpForce));
+    }
 }
---
+++ Assets/Script/Monster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster: Character
+{
+    public override void Awake()
+    {
+        base.Awake();
+        hp = 3;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        AliveOrDie();
+    }
+}
