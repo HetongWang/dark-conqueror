3455f492dc8db8bdbfeb72092543cee8a427ba27
Tonghe Wang <hetong583@qq.com>
2015-10-21 03:17:05 -0700

    frist playable

--- Assets/Script/NormalAttack.cs
+++ Assets/Script/NormalAttack.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 public class NormalAttack : BasicAttack {
-    static public float Ruration = 0.6f;
+    static public float Ruration = 0.2f;
+    static public float CD = 0.5f;
 
     public override void Awake()
     {
         _duration = Ruration;
+        _cd = CD;
         base.Awake();
     }
 }
