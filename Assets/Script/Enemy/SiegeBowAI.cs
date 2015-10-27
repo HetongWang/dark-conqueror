470a2804ea9e5f1e75122fb2b5b72fbb9ea793e6
Tonghe Wang <hetong583@qq.com>
2015-10-27 12:22:20 -0700

    fix angle bbug

--- Assets/Script/Enemy/SiegeBowAI.cs
+++ Assets/Script/Enemy/SiegeBowAI.cs
@@ -2,7 +2,7 @@
 
 public class SiegeBowAI : BasicAI
 {
-    public float angle = 30f;
+    public float angle = Mathf.Deg2Rad * 30;
     public float error = 0.2f;
 
     public SiegeBowAI(Character siegebow) : base(siegebow)
@@ -12,7 +12,7 @@ public class SiegeBowAI : BasicAI
 
     public Vector2 shootVelocity()
     {
-        float h = person.transform.position.y - targetPlayer.transform.position.y;
+        float h = -(person.transform.position.y - targetPlayer.transform.position.y);
         float d = person.transform.position.x - targetPlayer.transform.position.x;
         float g = Physics2D.gravity.y;
 
@@ -21,6 +21,8 @@ public class SiegeBowAI : BasicAI
         float vx = d * Mathf.Sqrt(g / (2 * (h - Mathf.Tan(angle) * d)));
         float vy = Mathf.Tan(angle) * vx;
         Vector2 v = new Vector2(vx, vy);
+
+        Debug.Log(h - Mathf.Tan(angle) * d);
 
         return v;
     }
