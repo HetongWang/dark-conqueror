19be329fea44145ebec9645bd15e62b3781db78e
Glen Cheng <strikerdrago@gmail.com>
2015-11-16 01:09:12 -0800

    Added end game and pause menu. Still needs some connecting. Pause is a prefab as well

--- Assets/Script/GUI/UIScript.cs
+++ Assets/Script/GUI/UIScript.cs
@@ -10,4 +10,14 @@ public class UIScript : MonoBehaviour {
 	public void ExitGame(){
 		//exit the game
 	}
+
+	public void SkillUp(){
+		//increase skill here
+		//based on soul count
+		//change text on button to +1
+	}
+
+	public void UnPause(){
+
+	}
 }
