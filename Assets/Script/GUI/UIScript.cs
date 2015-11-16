b9e7f2fa855d53e7f921bae7133a1a01281f03f8
Glen Cheng <strikerdrago@gmail.com>
2015-11-15 21:29:09 -0800

    Added main menu buttons plus simple functions.

---
+++ Assets/Script/GUI/UIScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScript : MonoBehaviour {
+
+	public void StartGame(){
+		Application.LoadLevel(1);
+	}
+
+	public void ExitGame(){
+		//exit the game
+	}
+}
