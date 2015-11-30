01b18c30f71a3c45868a7ecd95cff694fad783e7
Tonghe Wang <hetong583@qq.com>
2015-11-30 14:55:33 -0800

    better code

---
+++ Assets/Script/Enemy/EnemySet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemySet : CharacterSet
+{
+    public Vector2 hpBarOffset;
+
+    public EnemySet()
+    {
+        hpBarOffset = new Vector2(0, 3.5f);
+    }
+}
