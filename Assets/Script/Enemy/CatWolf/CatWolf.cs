20d30144fceed4c72a852a14d65a30cee66f3808
Tonghe Wang <hetong583@qq.com>
2015-12-07 04:08:43 -0800

    worth to try

--- Assets/Script/Enemy/CatWolf/CatWolf.cs
+++ Assets/Script/Enemy/CatWolf/CatWolf.cs
@@ -91,6 +91,7 @@ public class CatWolf : Enemy
     public IEnumerator alert()
     {
         anim.SetInteger("skill", (int)Ability.alert);
+        antiStaggerTime = setting.alert.actDuration;
         CatWolfAI _ai = (CatWolfAI)ai;
         _ai.alerted = true;
         yield return new WaitForSeconds(setting.alert.actDuration);
